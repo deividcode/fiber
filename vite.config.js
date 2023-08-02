ac1665f724901efb6f2e7745d51d266d50fc985f
Jose Quintero <quinteromda@gmail.com>
2023-08-02 12:20:59 -0400

    Add: plugin legacy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
   base: "https://deividcode.github.io/fiber/",
   build: {
     minify: false
-  }
+  },
+  plugins: [
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+  ],
 })
