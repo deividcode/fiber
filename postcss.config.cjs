d1b136374a39c6dfa0526a55ae50881b550aeab7
Jose Quintero <quinteromda@gmail.com>
2023-08-09 15:19:13 -0400

    agregando purgecss, mejorando la accesibilidad

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,14 +1,14 @@
-// const purgecss = require('@fullhuman/postcss-purgecss');
+const purgecss = require('@fullhuman/postcss-purgecss');
 const autoprefixer = require('autoprefixer');
 const postcssNested = require('postcss-nested');
 
 
 module.exports = {
   plugins: [
-    // purgecss({
-    //   content: ['index.html'],
-    //   css: ["src/**/*.css"]
-    // }),
+    purgecss({
+      content: ['index.html'],
+      css: ["src/**/*.css"]
+    }),
     autoprefixer(),
     postcssNested()
   ]
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
-import legacy from '@vitejs/plugin-legacy'
+import { defineConfig } from 'vite';
+import legacy from '@vitejs/plugin-legacy';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+
 
 export default defineConfig({
   base: "https://deividcode.github.io/fiber/",  
+  build: {
+    minify: false
+  },
   plugins: [
     legacy({
       targets: ["defaults"]
+    }),
+    ViteImageOptimizer({
+      /* pass your config */
     }),
   ],
 })
