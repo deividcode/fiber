88efaf32259a36c2d48e1611bae333cb4d589436
Jose Quintero <quinteromda@gmail.com>
2023-08-10 21:01:50 -0400

    Mejorando README, agregando imagen de preview

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
 import './src/css/reset.css';
 import './src/css/styles.css';
 
+// scrollElements.forEach((el) => {
+//   console.log(el);
+//   el.style.opacity = 0
+// })
+
+const elAnimation = document.querySelectorAll('.animate__animated');
+
+let observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animate__fadeInUp');    
+    }
+  });
+},{
+  threshold: 0.3
+});
+
+elAnimation.forEach(el => {
+
+  observer.observe(el);  
+});
+
